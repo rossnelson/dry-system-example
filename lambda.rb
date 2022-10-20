a1fdac2f70b19664359e669d18c12d9737055b4b
Ross Nelson <axcess1@me.com>
2022-10-20 10:25:54 -0400

    comments

--- lambda.rb
+++ lambda.rb
@@ -3,6 +3,8 @@
 require_relative 'system/boot'
 
 # something here to define the "middleware" to load for this function
+# is that required with dependency injection? I would assume the dependencies
+# are lazy loaded.
 
 def handler(event:, context:)
   puts "autoloaded: #{Container.keys}"
